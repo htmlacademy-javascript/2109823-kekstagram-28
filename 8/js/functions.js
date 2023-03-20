9bb2dea82f51e21b0b217140c2526b9fa15d35d4
Keks <keks@htmlacademy.ru>
2023-03-20 23:27:21 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/functions.js
@@ -0,0 +1,71 @@
+export const isValidateString = (str,len) => {
+  if(str.length > len){
+    return false;
+  }
+  return true;
+};
+
+export const isPalindrom = (str) => {
+  const result = str.toLowerCase().trim();
+  for(let i = 0; i < result.length / 2; i++){
+    if(result[i] !== result[result.length - 1 - i]){
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+export const extractNumber = (str) => {
+  const numbers = str.match(/\d+/g);
+  if(numbers === null){
+    return NaN;
+  }
+  return Number(numbers.join(''));
+};
+
+export const padStart = (str,minLength,strAdd) => {
+  if(str.length >= minLength){
+    return str;
+  }
+  const addLength = minLength - str.length;
+  if(addLength < strAdd.length){
+    strAdd = strAdd.slice(0,addLength);
+  }
+
+  return strAdd.repeat(Math.ceil(addLength / strAdd.length)).slice(0,addLength) + str;
+};
+
+export const getRandomNumber = (limit) => Math.floor(Math.random() * limit) + 1;
+
+export const getRandomLikes = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// функция для генерации случайного id (реализована в виде замыкания)
+export const generateId = (min, max) =>{
+  const usedIds = {};
+
+  return () => {
+    let id;
+    do {
+      id = getRandomLikes(min, max);
+    } while (usedIds[id]);
+    usedIds[id] = true;
+    return id;
+  };
+};
+
+// функция для случайного url для фотографии
+export const generateUrl = () => {
+  const uniqValue = generateId(1, 25);
+  const idPhotoUrl = uniqValue();
+  return `photos/${idPhotoUrl}.jpg`;
+};
+
+// функция для случайного url для аватара
+export const getAvatar = () => {
+  const uniqValue = generateId(15, 200);
+  return `img/avatar-${uniqValue()}.svg`;
+};
+
+
